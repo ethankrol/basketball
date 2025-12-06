4dc6dd8a10f42b94b6dd2862143ebae3e8051b33
ethankrol <ethanmkrol@gmail.com>
2025-12-06 17:07:42 -0500

    Add combined dataframe for unranked and ranked teams

--- etl/ap_poll_data.py
+++ etl/ap_poll_data.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from dotenv import load_dotenv
 import pandas as pd
+import os
 
 load_dotenv()
 headers = {
@@ -11,14 +12,13 @@ headers = {
     "Referer": "https://example.com",
 }
 
-for year in range(2003, 2027):
+for year in range(int(os.environ.get('FIRST_SEASON_ESPN')), int(os.environ.get('CUR_SEASON_ESPN')) + 1):
     week = 1
 
 response = requests.get('https://www.espn.com/mens-college-basketball/rankings/_/week/9/year/2014/seasontype/2', headers=headers)
 html_content = response.text
 
 df = pd.read_html(html_content, flavor="bs4")[0]
-print(df.head(5))
 
 soup = BeautifulSoup(html_content, 'html.parser')
 trends = []
@@ -26,7 +26,6 @@ trs = soup.find_all('tr')
 for tr in trs:
     for td in tr.find_all('td'):
         for div in td.find_all('div'):
-            print(div)
             cls = div.get('class')
             trend = div.text
             if 'positive' in cls:
@@ -36,23 +35,58 @@ for tr in trs:
             elif 'trend' in cls:
                 trends.append(0)
 
+# Find the "others receiving votes" text
+other_teams = []
+paragraphs = soup.find_all('p')
+for p in paragraphs[0:2]:
+    span = p.find_all('span')
+    for s in span:
+        if "Others" in s.text:
+            teams_text = p.text.split(':')[1]
+            teams_split = teams_text.strip().split(', ')
+            for team in teams_split:
+                split_team_score = team.split(' ')
+                team = ' '.join(split_team_score[:-1])
+                score = int(split_team_score[-1])
+                other_teams.append((team, score, 0))
+
+other_teams.sort(key = lambda x : x[1], reverse=True)
+
+other_df = pd.DataFrame(other_teams, columns = ['Team', 'PTS', 'first'])
+print(other_df)
 
 # Subset the first 25 (AP Poll only, everything after is coaches poll)
 trends = trends[:25]
 
 # Replace unsigned trends with positive-negative trends
-df['TREND'] = trends
+#df['TREND'] = trends
 
 # Find first place votes
 df['first'] = df['Team'].str.findall('\((\d+)\)').str[0]
 df['first'].fillna(0, inplace=True)
-df['abv'] = df['Team'].str.split(' ').str[0]
+
+# Team abbreviations
+#df['abv'] = df['Team'].str.split(' ').str[0]
+
+# Reformatted team name
 df['Team'] = df['Team'].str.replace('\(\d+\)', '', regex=True).str.rstrip().str.split(' ').str[1:].str.join(' ')
-df['wins'] = df['REC'].str.split('-').str[0].astype(int)
-df['losses'] = df['REC'].str.split('-').str[1].astype(int)
+
+# Team wins
+#df['wins'] = df['REC'].str.split('-').str[0].astype(int)
+
+# Team losses
+#df['losses'] = df['REC'].str.split('-').str[1].astype(int)
+#df['season'] = season
+#df['week'] = week
+
+combined_df = pd.concat((df[['Team', 'PTS', 'first']], other_df))
+combined_df = combined_df.rename(columns = {'Team': 'team', 'PTS': 'votes', 'first': 'first_votes'})
+combined_df['rank'] = combined_df['votes'].rank(method='min', ascending=False).astype(int)
+combined_df.reset_index(drop=True, inplace=True)
+print(combined_df.head(45))
 #df.rename(columns = {'RK': 'rank', 'Team': 'team', 'REC'})
 
-print(df.head(15))
+#print(df.head(15))
 
 
 #if not rows:
