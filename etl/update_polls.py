6de3c1043118be18148b7789efb73620355aa1fa
ethankrol <ethanmkrol@gmail.com>
2025-12-16 14:00:43 -0600

    clean update_polls.py

--- etl/update_polls.py
+++ etl/update_polls.py
@@ -1,28 +1,8 @@
-import requests
-import csv
-import os
 import pandas as pd
-from supabase import create_client
 from datetime import datetime, date
 from dotenv import load_dotenv
-import json 
-from io import StringIO
 from data_agg import upsert_df_into_db
 from ap_poll_data import APPollManager
-
-def get_current_season():
-    today = datetime.now()
-    month = int(today.month)
-    year = int(str(today.year)[-2:])
-
-    if month > 10:
-        return year + 1
-    return year 
-
-def get_current_week(season_start_date: date) -> int:
-    """Given the start date of a season, return the current week as a date starting on Monday."""
-    today = datetime.now().date()
-    return(today)
 
 def insert_current_week():
     ap = APPollManager()
